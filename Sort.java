06c22deebb9168bb18bec4700b6e1a4be34ef5dc
gjcsup <47387630+gjcsup@users.noreply.github.com>
2019-05-06 18:09:14 +0200

    init

---
+++ Sort.java
@@ -0,0 +1,39 @@
+// Name:
+// Student number:
+import java.util.Arrays;
+
+public class Sort
+{
+	
+	////// Implement the functions below this line //////
+	
+	/********** MERGE **********/
+	public static <T extends Comparable<? super T>> void mergesort(T[] data, int first, int last, boolean debug)
+	{
+
+		// Your code here
+
+	}
+     
+	private static <T extends Comparable<? super T>> void merge(T[] data, int first, int last, boolean debug)
+	{
+
+		// Your code here
+        
+		//DO NOT MOVE OR REMOVE!
+		if (debug)
+			System.out.println(Arrays.toString(data));
+	}
+     
+	/********** COUNTING **********/
+	public static void countingsort(int[] data, boolean debug)
+	{
+
+		// Your code here
+
+		//DO NOT MOVE OR REMOVE!
+		if (debug)
+			System.out.println(Arrays.toString(data));
+	}
+
+}
